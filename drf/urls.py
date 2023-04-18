69e1955eed55fdadba043f6d620a26893460a3a2
Patryk <patrykprause@gmail.com>
2023-04-18 20:00:38 +0200

    modified:   drf/settings.py 	modified:   drf/urls.py 	new file:   posts/__init__.py 	new file:   posts/admin.py 	new file:   posts/apps.py 	new file:   posts/migrations/0001_initial.py 	new file:   posts/migrations/__init__.py 	new file:   posts/models.py 	new file:   posts/serializers.py 	new file:   posts/tests.py 	new file:   posts/views.py

--- drf/urls.py
+++ drf/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from posts.views import PostView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/posts/', PostView.as_view(), name='post-list')
+
 ]
