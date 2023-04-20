6cee9fc0315879f616cd5f59d69c07d6e3b2a7d4
Patryk <patrykprause@gmail.com>
2023-04-20 20:54:23 +0200

    class based api view

--- drf/urls.py
+++ drf/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/posts/', PostView.as_view(), name='post-list'),
-    path('api/post-list/', post_list, name='post-list'),
+    path('api/posts/<int:pk>/', PostView.as_view(), name='post-list'),
+
+    # path('api/post-list/', post_list, name='post-list'),
     path('api/posts/<int:pk>/', post_detail, name='post-detail')
 
 ]
