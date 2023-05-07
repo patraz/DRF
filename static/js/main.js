12b399d99dd91409f583554e38bf0790d22b5fee
Patryk <patrykprause@gmail.com>
2023-05-07 16:56:33 +0200

    list views p2

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,51 @@
 console.log('doopa')
+function getPostList(){
+    fetch('/api/posts/')
+    .then(res => res.json())
+    .then(data => {
+        renderPosts(data)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+}
+
+function renderPosts(data) {
+    return data.map(post => {
+        renderPost(post);
+    })
+}
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el)
+}
+
+function renderPost(post) {
+    const root = document.getElementById('root')
+    const div = createNode('div');
+    div.className = 'post-item';
+    const title = createNode('h2');
+    const content = createNode('p');
+    const publishDate = createNode('span')
+    const lastUpdated = createNode('span')
+    const author = createNode('small');
+
+    author.innerText = post.author;
+    content.innerText = post.content;
+    title.innerText = `${post.title} Written by: ${post.author}`;
+    publishDate.innerText = `Published: ${new Date(post.publish_date).toUTCString()}`;
+    lastUpdated.innerText = `Last Updated: ${new Date(post.updated).toUTCString()}`;
+
+    append(div, title);
+    append(div, content);
+    append(div, publishDate);
+    append(div, lastUpdated);
+    append(root, div);
+}
+
+
+getPostList()
